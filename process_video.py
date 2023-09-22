2a0c1a073142cd65b64086a4ba0b5ec0ec570509
Christopher Alesevich <christopher.alesevich@gmail.com>
2023-09-22 12:15:43 -0400

    Script functionality updates

--- process_video.py
+++ process_video.py
@@ -1,103 +1,147 @@
-from manim import *
-from manim.utils.rate_functions import linear
 import cv2
-import numpy as np
+from time import sleep
+from manim import Scene, Text, Circle, Arrow, config
+from manim.animation.creation import Create
 from moviepy.editor import VideoFileClip, concatenate_videoclips, AudioFileClip
 
-def process_scene(scene_name, image_pairs, output_filename, total_length_minutes, total_length_seconds):
+
+RED = "#FF0000"
+GREEN = "#00FF00"
+BLUE = "#0000FF"
+YELLOW = "#FFFF00"
+PURPLE = "#800080"
+
+
+def detect_objects(image_path):
+    img = cv2.imread(image_path)
     detected_items = []
-    image1 = cv2.imread(image_pairs[0])
-    
-    for color, (scene, item_type, appear_time, disappear_time, custom_text, font_name, font_size, output_color) in color_dictionary.items():
-        if scene != scene_name and scene != 'all':
-            continue
-        lower_bound = np.array(color, dtype=np.uint8)
-        upper_bound = np.array(color, dtype=np.uint8)
-        mask = cv2.inRange(image1, lower_bound, upper_bound)
+
+    for item, attributes in color_dictionary.items():
+        mask = cv2.inRange(img, attributes["color"], attributes["color"])
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
+
         for contour in contours:
             x, y, w, h = cv2.boundingRect(contour)
-            detected_items.append((item_type, appear_time, disappear_time, custom_text, font_name, font_size, output_color, x, y, w, h))
-            
-    class AnimateDetectedItems(Scene):
-        def construct(self):
-            current_time = 0
-            aspect_ratio = 800 / 600
-            second_image = ImageMobject(image_pairs[1]).scale(0.5)
-            self.add(second_image)
-            
-            for item_type, appear_time, disappear_time, custom_text, font_name, font_size, output_color, x, y, w, h in detected_items:
-                manim_x = (x - 400) / 400 * aspect_ratio
-                manim_y = (300 - y) / 300
-                appear_minutes, appear_seconds = map(int, appear_time.split(":"))
-                appear_time_seconds = appear_minutes * 60 + appear_seconds
-                disappear_minutes, disappear_seconds = map(int, disappear_time.split(":"))
-                disappear_time_seconds = disappear_minutes * 60 + disappear_seconds
-                self.wait(appear_time_seconds - current_time)
-                
-                if item_type == "text":
-                    obj = Text(custom_text, font=font_name).move_to([manim_x, manim_y, 0]).scale(font_size).set_color(output_color)
-                    self.play(Create(obj, rate_func=linear))
-                elif item_type == "arrow":
-                    obj = Arrow(start=[manim_x, manim_y, 0], end=[manim_x + w / 800, manim_y - h / 600], color=output_color)
-                elif item_type == "circle":
-                    obj = Circle().move_to([manim_x, manim_y, 0]).set_width(w / 800).set_height(h / 600).set_color(output_color)
-                elif item_type == "oval":
-                    obj = Ellipse(width=w / 800, height=h / 600).move_to([manim_x, manim_y, 0]).set_color(output_color)
-                elif item_type == "question_mark":
-                    obj = Text("?", font=font_name).move_to([manim_x, manim_y, 0]).scale(font_size).set_color(output_color)
-                    self.play(Create(obj, rate_func=linear), Rotate(obj, angle=2*PI))
-                
-                self.play(ShowCreation(obj))
-                self.wait(disappear_time_seconds - appear_time_seconds)
-                self.play(FadeOut(obj))
-                current_time = disappear_time_seconds
-    
-    config.file_writer_config["output_file"] = output_filename
-    scene = AnimateDetectedItems()
+            center = (int((2 * x + w) / 2), int((2 * y + h) / 2))
+            detected_items.append({
+                "type": item,
+                "center": center,
+                "size": (w, h),
+                "scene_name": attributes["scene_name"]
+            })
+
+    return detected_items
+
+
+class ProcessedImageScene(Scene):
+    def __init__(self, detected_items, image_path, total_length_minutes, total_length_seconds, *args, **kwargs):
+        self.detected_items = detected_items
+        self.image_path = image_path
+        self.total_length_minutes = total_length_minutes
+        self.total_length_seconds = total_length_seconds
+        super().__init__(*args, **kwargs)
+
+    def construct(self):
+        for item in self.detected_items:
+            obj = None
+            attributes = color_dictionary.get(item["type"], {})
+
+            if "scene_name" in attributes:
+                scene = attributes["scene_name"]
+                if scene != self.name and scene != 'all':
+                    continue
+
+            start_time = attributes.get("start_time", 0)
+            end_time = attributes.get("end_time", self.total_length_minutes * 60 + self.total_length_seconds)
+
+            if item["type"] == "text":
+                obj = Text("Your Text Here", font=attributes["font"], size=attributes["size"]).set_color(
+                    attributes["output_color"]).move_to(item["center"])
+                self.play(Create(obj), rate_func=lambda t: smooth(1 if start_time <= t <= end_time else 0))
+
+            elif item["type"] == "circle":
+                obj = Circle(radius=item["size"][0] / 2).set_color(attributes["output_color"]).move_to(item["center"])
+                self.play(Create(obj), rate_func=lambda t: smooth(1 if start_time <= t <= end_time else 0))
+
+            elif item["type"] == "arrow":
+                obj = Arrow(start=item["center"],
+                            end=(item["center"][0], item["center"][1] + item["size"][1])).set_color(
+                    attributes["output_color"])
+                self.play(Create(obj), rate_func=lambda t: smooth(1 if start_time <= t <= end_time else 0))
+
+            elif item["type"] == "question_mark":
+                obj = Text("?").set_color(attributes["output_color"]).scale(3).move_to(item["center"])
+                self.play(obj.animate.rotate(PI / 2).set_color(attributes["output_color"]),
+                          rate_func=lambda t: smooth(1 if start_time <= t <= end_time else 0))
+
+        self.wait(self.total_length_minutes * 60 + self.total_length_seconds)
+
+
+def process_scene(scene_name, image_pairs, output_filename, total_length_minutes, total_length_seconds):
+    detected_items = detect_objects(image_pairs[0])
+    config.output_file = output_filename
+    scene = ProcessedImageScene(detected_items, image_pairs[1], total_length_minutes, total_length_seconds)
     scene.render()
 
-# Manim Configuration
-config.pixel_height = 1080
-config.pixel_width = 1920
-config.frame_height = 8.0
-config.frame_width = 14.22
 
-# Color Dictionary
+def concatenate_scenes(scene_filenames, audio_file, output_filename):
+    clips = [VideoFileClip(scene_filename) for scene_filename in scene_filenames]
+    final_clip = concatenate_videoclips(clips)
+    audio = AudioFileClip(audio_file)
+    final_clip = final_clip.set_audio(audio)
+    final_clip.write_videofile(output_filename)
+
+
 color_dictionary = {
-    (255, 0, 0): ('all', 'text', "0:05", "0:07", "Hello", "Arial", 0.5, RED),
-    (0, 255, 0): ('scene1', 'arrow', "0:10", "0:12", "", "", 0, GREEN),
-    (0, 0, 255): ('scene2', 'circle', "0:15", "0:17", "", "", 0, BLUE),
-    (255, 255, 0): ('scene2', 'oval', "0:20", "0:22", "", "", 0, YELLOW),
-    (0, 255, 255): ('all', 'question_mark', "0:25", "0:27", "", "Arial", 0.5, CYAN)
-}
-
-# Scenes to Process
-scenes_to_process = {
-    "scene1": {
-        "image_pairs": ("image1_for_detection.png", "image1_for_annotation.png"),
-        "output_filename": "output_scene1",
-        "total_length": ("0", "30")
+    "text": {
+        "color": (255, 0, 0),
+        "font": "Times New Roman",
+        "size": 1,
+        "output_color": RED,
+        "start_time": 5,
+        "end_time": 15,
+        "scene_name": "all"
     },
-    "scene2": {
-        "image_pairs": ("image2_for_detection.png", "image2_for_annotation.png"),
-        "output_filename": "output_scene2",
-        "total_length": ("0", "30")
+    "circle": {
+        "color": (22,154,185),
+        "output_color": GREEN,
+        "start_time": 10,
+        "end_time": 20,
+        "scene_name": "scene1"
+    },
+    "arrow": {
+        "color": (0, 0, 255),
+        "output_color": BLUE,
+        "start_time": 15,
+        "end_time": 25,
+        "scene_name": "scene2"
+    },
+    "question_mark": {
+        "color": (255, 255, 0),
+        "output_color": YELLOW,
+        "start_time": 20,
+        "end_time": 30,
+        "scene_name": "scene1"
     }
 }
 
-# Process Each Scene
-video_files = []
-for scene_name, scene_details in scenes_to_process.items():
-    image_pairs = scene_details["image_pairs"]
-    output_filename = scene_details["output_filename"]
-    total_length_minutes, total_length_seconds = scene_details["total_length"]
-    process_scene(scene_name, image_pairs, output_filename, total_length_minutes, total_length_seconds)
-    video_files.append(f"{output_filename}.mp4")
+scenes_to_process = {
+    "scene1": {
+        "image_pairs": ("image1_drawing.png", "image1_raw.png"),
+        "output_filename": "scene1_output.mp4",
+        "total_length_minutes": 0,
+        "total_length_seconds": 30
+    }
+}
 
-# Merge Videos and Add Audio
-final_audio = "your_audio.mp3"
-final_clip = concatenate_videoclips([VideoFileClip(f) for f in video_files])
-final_clip.audio = AudioFileClip(final_audio).subclip(0, final_clip.end)
-final_clip.write_videofile("final_output.mp4")
+if __name__ == "__main__":
+    scene_outputs = []
+    for scene_name, scene_data in scenes_to_process.items():
+        process_scene(scene_name, scene_data["image_pairs"], scene_data["output_filename"],
+                      scene_data["total_length_minutes"], scene_data["total_length_seconds"])
+        scene_outputs.append(scene_data["output_filename"])
+
+    print("Sleeping")
+    sleep(10)
+
+    concatenate_scenes(scene_outputs, "audio.mp3", "final_output.mp4")
